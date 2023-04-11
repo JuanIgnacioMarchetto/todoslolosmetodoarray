b2d29a20583c6788df980b8140ed4bc4f86a5c14
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-04-11 13:37:03 -0300

    11/4/2023

---
+++ cursojavascript.js
@@ -0,0 +1,357 @@
+//alert("hola mundo");
+
+/*alert("hola mundo desde javascript");
+var numero = 5;
+
+Var suma = 5+5;
+var resta =9 - 9;
+var multiplicacion = 9*5;
+var division = 50/5;*/
+
+/*tipos de variables:
+
+var
+let
+const*/
+
+/* tipos de datos:
+
+number
+bigint
+string
+booleanos
+null
+undefined*/
+
+/*declaracion de variables
+var es una palabra reservada para declarar una variable-
+var nombrreVariable
+
+let es una palabra reservada para declarar una variable pero q se puede usar dentro de determinados bloques de codigo(generalmente limitados por llaves{})("bloque de codigo")
+let nombreVariable;
+
+const eses una palabra reservada para declarar una variable constante q se puede usar dentro de determinados bloques de codigo(generalmente limitados por llaves{})("bloque de codigo")..cuando
+le asignamos un valor, no se puede cambiar el mismo durante la ejecucion del programa
+
+const nombreVariable*/
+/*
+
+var nombre ="Juani";
+var edad="36";
+var presenteONo=true;
+var apellido="Marchetto";
+var estatura=1.74;
+
+//alert("esta pagina es una prueba de conocimientos de "+ nombre);
+
+console.log ("nombre: " + nombre +" "+ typeof nombre);
+console.log ("edad: " + edad+" "+ typeof edad);
+console.log ("presente: " + presenteONo +" "+ typeof presenteONo);
+console.log ("apellido: " + apellido+" "+ typeof apellido);
+console.log ("estatura: " + estatura +" "+ typeof estatura);
+
+
+¿¿¿¿¿¿¿¿¿¿¿¿¿¿var info = (
+    "nombre: " + nombre +" "+ typeof nombre,
+    "edad: " + edad+" "+ typeof edad,
+    "presente: " + presenteONo +" "+ typeof presenteONo,
+    "apellido: " + apellido+" "+ typeof apellido,
+    "estatura: " + estatura +" "+ typeof estatura,????????????
+    
+ */
+
+/*     if (true){
+    var nombre="ricardo";
+}
+console.log(nombre) */
+
+/* if (true){
+    let nombre="ricardo";
+}
+console.log(nombre)
+
+¡¡¡¡¡¡ReferenceError: nombre is not defined!!!!!! */
+
+/* 
+const email="pedroargento@gmail.com";
+
+email="otroemail@com";
+
+¡¡¡¡¡¡¡¡¡¡¡¡¡TypeError: Assignment to constant variable.!!!!!!!!1 */
+
+//operadores en javascript 
+/*
+asignacion
+aritmetica
+condicional
+comparacion*/
+
+//aritmetica:
+/*25+50//suma
+35-14//resta
+500/5//divide
+10++//incrementa uno
+10--//decrementa uno
+30%2//resto de la division(en este ejemplo 0)
+
+*/
+//comparacion
+/*
+igualdad ==
+desigual !=
+igualdad estricta ===
+desigualdad estricta !==
+mayor y menor ><
+mayor o igual y menor o igual >= <=
+*/
+
+//condiciones 
+/*
+and &&: se utiliza cuando se debe cumplir ambas condiciones
+
+or || : se utiliza cuando se debe cumplir una condicion o la otra.
+
+not ! : se utiliza para negar el valor de una condicion (si es true pasa a false y viceversa)*/
+
+/*var suma= 35+5;
+console.log(suma
+    )
+var resta=20-4;
+console.log(resta
+    )
+var multi=5*5;
+console.log(multi
+    )
+var divi=30/2;
+console.log(divi
+    )
+var resto =60%5;
+console.log(resto
+    )
+*/
+
+//comparacion
+/*
+console.log(5>2);
+console.log(98<1);
+console.log(4==4);
+var numero=9;
+console.log(4==numero);*/
+/*var edad= 18;
+var dni=!false;
+**/
+//ejemplo AND
+/*
+console.log (edad>=18 && dni==true);
+console.log (edad>=18 && dni==false);
+ejemplo or
+
+console.log (edad>=16 || dni==true);
+console.log (edad>=18 || dni==false);
+*/
+
+//console.log(dni)
+
+/*var totalComida=2800+1300;
+var totalbebida=900+650;
+
+var total= totalComida + totalComida;
+ 
+console.log ( "el total de bebida : " + totalbebida)
+console.log ( "el total de comida : " + totalComida)
+
+console.log ( "el total de la cuenta es : " + total)
+*/
+///condicionales
+/*
+if :
+permite  decidir entre dos opciones resultantes de la evaluacion de una sentencia
+si la evaluacion es positiva (true)ejecuta una parte del codigo
+if (condicion){
+    codigo q se ejecuta si se cumple la condicion
+}
+
+if + else
+en caso q no se cumpla una condicion, if permite agregar una condicion else q incluye un bloque de codigo q se ejecuta al no cumplirce la primer condicion
+*/
+/*if (condicion){
+    codigo que se ejecuta si se cumple la condicion
+}else{
+    codigo q se ejecuta si no se ejecuta la condicion
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
