908cde6b6083786f908577ded8661ea21f206cf1
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-04-12 08:09:16 -0300

    12/4/2023-12/4/2023-8:09

--- while.js
+++ while.js
@@ -5,8 +5,22 @@ la condicion se evalua siemprealinicio del ciclo/bucle
 si lacondicion es valida,ingresa al bucle y ejecuta el codigo
 cuando deja de cumplirse la condicion , el programa sale del ciclo y continua con el resto del programa*/
 
+/*
+var contador = 0;
+while(contador <= 10){
+    console.log("vuelta n° "+contador);
+    contador ++;*/
+
+/*???????????????????????????????????????????????????????????'
+var numero =prompt("ingresa un numero")
+var contador=numero;
 
 
+while(contador <= 10){
+    
+    document.write("<h1>vuelta n°</h1>"+contador);
+    contador ++;
+}*/
 
 
 
