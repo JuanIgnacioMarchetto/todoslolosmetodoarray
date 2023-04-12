1d9e09a01b67cfc5843339720d488bf6deded2f7
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-04-12 08:50:19 -0300

    12/4/2023-12/4/2023-8:50

---
+++ for.js
@@ -0,0 +1,13 @@
+//for
+/*
+realiza un bucle una determinada cantidad de veces. es un bucle controlado por un contador, q de hecho , lo tiene implicito en su sintaxis.
+el for esta compuesto por 3partes:
+
+inicializacion de la variable q utilizamos en la condicion )(se ejecuta solo una vez al principio del ciclo).
+condicion de fin de ciclo (se evalua esta expresion al comienzo de cada iteracion).
+modificacion de la variable(se ejecuta al final de cada iteracion).
+
+*/
+for (var i=1;i<10; i++){
+    console.log("vuelta numero " + i)
+};
