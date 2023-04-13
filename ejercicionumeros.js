845b9912de8f7da8bf87b888e9b717d0ecad34bf
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-04-13 09:28:36 -0300

    13/4/2023-09:28

---
+++ ejercicionumeros.js
@@ -0,0 +1,16 @@
+/*
+var contador = 1;
+do {
+    console.log("n°: "+ contador);
+    contador++;
+
+}while(contador <=35)
+*/
+/*
+for (var i=1;i<=35; i++){
+    console.log("n°" + i)
+};*/
+
+for (var i=10;i>=0; i--){
+    console.log( i);}
+alert("FEliz año nuevo")
